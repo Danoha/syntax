dfea7eec615fa5091d5c298139b5be0e4234ec56
Danoha <dan.stribrny@gmail.com>
2014-06-27 21:39:16 +0200

    Basic chat functionality

--- server/lib/server.js
+++ server/lib/server.js
@@ -14,9 +14,27 @@ var Server = function(credentials) {
   
   this.https = https.createServer(opts);
   this.io = SocketIO.listen(this.https);
+  
+  var self = this;
+  this.io.on('connection', function(socket) {
+    console.log('a user connected');
+    socket.on('disconnect', function() {
+      console.log('user disconnected');
+    });
+    
+    socket.on('chat message', function(msg) {
+      console.log('message: ' + msg);
+      
+      self.io.emit('chat message', msg);
+    });
+  });
+  
+  
 };
 
 Server.prototype.listen = function(port) {
+  console.log("listening on *:" + port);
+  
   this.https.listen(port);
 };
 
---
+++ web-client/js/app.js
@@ -0,0 +1,52 @@
+/* 
+ * Copyright (C) 2014 Danoha
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+(function() {
+  'use strict';
+  
+  var App = function() {
+    this.server = 'https://127.0.0.1:1560';
+
+    var self = this;
+    $.getScript(this.server + '/socket.io/socket.io.js', function() {
+      self.io = io(self.server);
+      
+      $(function() {
+        self.init();
+      });
+    });
+  };
+
+  App.prototype.init = function() {
+    var self = this;
+    $('form.message-form').submit(function() {
+      var input = $('input.message');
+
+      self.io.emit('chat message', input.val());
+      
+      input.val('');
+      return false;
+    });
+    
+    this.io.on('chat message', function(msg) {
+      $('.messages').prepend($('<div class="message">').text(msg));
+    });
+  };
+
+  document.syntaxApp = new App();
+})();
